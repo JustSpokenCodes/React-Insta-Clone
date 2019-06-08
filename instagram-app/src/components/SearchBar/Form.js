a6fe6b973c6c1ea3aee2e48c971a58fe49569db4
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-06-08 09:53:03 -0700

    minor fix

--- instagram-app/src/components/SearchBar/Form.js
+++ instagram-app/src/components/SearchBar/Form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Form.css';
 
- const Form = ({ inputValue, changeHandler, addComment }) => {
+const Form = ({ inputValue, changeHandler, addComment }) => {
     return (
     <form onSubmit={(event) => addComment(event)}>
         <input
@@ -14,4 +14,4 @@ import './Form.css';
     </form>)
 }
 
- export default Form;
+export default Form;
