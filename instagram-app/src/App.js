97a8a740c7428f4ee12359db9f1f5ce78f90171f
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-06-08 10:35:40 -0700

    try to get the comment section to look correct

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import data from './dummy-data';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
--- instagram-app/src/components/PostContainer/postContainer.js
+++ instagram-app/src/components/PostContainer/postContainer.js
@@ -1,33 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
+import uuidv4 from "uuid/v4"
 import './PostContainer.css';
 import { IoIosHeartEmpty, IoIosCompass } from 'react-icons/io';
 import CommentSection from '../CommentSection/CommentSection';
+import Form from "../SearchBar/Form";
 
- const PostContainer = (props) => {
+const PostContainer = ({props}) => {
+    const { comments, thumbnailUrl, imageUrl, timestamp, likes, username } = props;
+    const [inputValue, setChange] = useState('');
+    const [inputComment, setComment] = useState(comments);
+    const handleChange = (e) =>{
+        setChange(e.target.value);
+    }
+    const postComment = (e) => {
+        e.preventDefault();
+        const newComment = {
+            id: uuidv4(),
+            username: "user",
+            text: inputValue
+        };
+        setComment(comments.concat(newComment));
+        setChange('');
+    }
+
     return (
         <div className="post-container">
             <div className="user-info">
-                <img className="profile-pic" src={props.post.thumbnailUrl} alt="user-profile" />
-                <p>{props.post.username}</p>
+                <img className="profile-pic" src={thumbnailUrl} alt="user-profile" />
+                <p>{username}</p>
             </div>
             <div className="user-post">
-                <img className="post-image" src={props.post.imageUrl} alt="user-post"/>
+                <img className="post-image" src={imageUrl} alt="user-post"/>
             </div>
             <div className="reaction">
                 <div className="post-icons">
                     <IoIosHeartEmpty /> <IoIosCompass/>
                 </div>
-                {props.post.likes} likes
+                {likes} likes
             </div>
-            {props.post.comments.map((comment, index) => {
-                console.log('==', comment);
-                return <CommentSection key={index} props= {comment} />
-            })}
+            { inputComment.map((comment) =>{
+                return <CommentSection key={comment.id} props = {comment}/>
+            })
+            }
             <div className="timestamp">
-                {props.post.timestamp}
+                {timestamp}
             </div>
+            <Form 
+                inputValue={inputValue}
+                changeHandler={handleChange}
+                addComment= {postComment}
+            />
         </div>
     )
 }
 
- export default PostContainer;
+
+export default PostContainer;
